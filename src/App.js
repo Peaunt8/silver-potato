9216198353fe3ec68bafba75c7386cd49dbf3486
何庚坤 <hegengkun.ahab@bytedance.com>
2022-04-06 23:15:06 +0800

    fix: router and profile

--- src/App.js
+++ src/App.js
@@ -1,33 +1,8 @@
-import {BrowserRouter, Route, Switch} from "react-router-dom";
-import MemberManagement from "./page/memberManagement/memberManagement";
-import PersonalPage from "./page/personalPage/personalPage";
-import TeamActivity from "./page/teamActivity/teamActivity";
-import TeamSignup from "./page/teamSignUp/teamSignUp"
-import SiderDemo from './siderDemo';
-import React from "react";
+import React from 'react';
+import SiderDemo from './page/siderDemo/siderDemo';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/personal_page">
-          <PersonalPage></PersonalPage>
-        </Route>
-        <Route path="/member_management">
-          <MemberManagement></MemberManagement>
-        </Route>
-        <Route path="/team_activity">
-          <TeamActivity></TeamActivity>
-        </Route>
-        <Route path='/team_sign_up'>
-          <TeamSignup></TeamSignup>
-        </Route>
-        <Route path="/">
-          <SiderDemo></SiderDemo>
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  );
+  return <SiderDemo />;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from "react";
-import * as ReactDOM from "react-dom";
-//引入样式
-import App from "./App";
-import "./index.css";
-import {BrowserRouter} from "react-router-dom";
-
-
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+// 引入样式
+import App from './App';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter><App />
-    </BrowserRouter>, document.querySelector('#root'));
-
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.querySelector('#root')
+);
